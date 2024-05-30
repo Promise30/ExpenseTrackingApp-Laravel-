a3150d698451bea7e9516c9673a456a370556b8f
Promise30 <promiseadeagbo@gmail.com>
2024-05-30 08:55:36 +0000

    task: initial expense model

---
+++ app/Models/Expense.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Expense extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'title',
+        'description',
+        'quantity',
+        'unit_price',
+        'category',
+        'receipt',
+        'user_id'
+    ];
+    public function getTotalCostAttribute()
+    {
+        return $this->quantity * $this->unit_price;
+    }
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
