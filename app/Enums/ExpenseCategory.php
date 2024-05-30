6b44b61b07ffa96b2e10881382d60f19256709ed
Promise30 <promiseadeagbo@gmail.com>
2024-05-30 08:59:57 +0000

    task: added enums for expense category

---
+++ app/Enums/ExpenseCategory.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+enum ExpenseCategory: string
+{
+    //
+    case FOOD = "food";
+    case UTILITIES = "utilities";
+    case TRANSPORTATION = "transportation";
+    case LEISURE = "leisure";
+    case OTHERS = "others";
+
+}
