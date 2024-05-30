1269ffd90ff555775f37ca58cefd9e72c6a44e3a
Promise30 <promiseadeagbo@gmail.com>
2024-05-30 08:56:38 +0000

    task: initial database migration schema for expense model

---
+++ database/migrations/2024_05_29_200357_create_expenses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\ExpenseCategory;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->unsignedInteger('quantity');
+            $table->decimal('unit_price');
+            $table->string('category')->default(ExpenseCategory::OTHERS->value);
+            $table->string('receipt')->nullable();
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expenses');
+    }
+};
