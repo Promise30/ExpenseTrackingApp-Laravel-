8eb6624b638c7007b1aac5e845897326aed4024d
Promise30 <promiseadeagbo@gmail.com>
2024-05-30 08:50:11 +0000

    task: added expense related routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExpenseController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +17,14 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// Expense related routes
+Route::get('expenses', [ExpenseController::class, 'index'])->name('expense.index');
+Route::get('expenses/create', [ExpenseController::class, 'create'])->name('expense.create');
+Route::post('expenses', [ExpenseController::class, 'store'])->name('expense.store');
+Route::get('expenses/{expense}', [ExpenseController::class, 'show'])->name('expense.show');
+Route::get('expenses/{expense}/edit', [ExpenseController::class, 'edit'])->name('expense.edit');
+Route::patch('expenses/{expense}', [ExpenseController::class, 'update'])->name('expense.update');
+Route::delete('expenses/{expense}', [ExpenseController::class, 'destroy'])->name('expense.destroy');
 
 require __DIR__.'/auth.php';
