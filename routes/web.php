1a16af0258c8a4f9a711465680ef055d81482249
Promise30 <promiseadeagbo@gmail.com>
2024-06-09 17:35:07 +0000

    task: defined notification related routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ExpenseController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\NotificationController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,14 +20,34 @@ Route::middleware('auth')->group(function () {
 });
 
 // Expense related routes
-Route::middleware('auth')->group(function () {
-Route::get('expenses', [ExpenseController::class, 'index'])->name('expense.index');
-Route::get('expenses/create', [ExpenseController::class, 'create'])->name('expense.create');
-Route::post('expenses', [ExpenseController::class, 'store'])->name('expense.store');
-Route::get('expenses/{expense}', [ExpenseController::class, 'show'])->name('expense.show');
-Route::get('expenses/{expense}/edit', [ExpenseController::class, 'edit'])->name('expense.edit');
-Route::patch('expenses/{expense}', [ExpenseController::class, 'update'])->name('expense.update');
-Route::delete('expenses/{expense}', [ExpenseController::class, 'destroy'])->name('expense.destroy');
+// Route::middleware('auth')->group(function () {
+// Route::get('expenses', [ExpenseController::class, 'index'])->name('expense.index');
+// Route::get('expenses/create', [ExpenseController::class, 'create'])->name('expense.create');
+// Route::post('expenses', [ExpenseController::class, 'store'])->name('expense.store');
+// Route::get('expenses/{expense}', [ExpenseController::class, 'show'])->name('expense.show');
+// Route::get('expenses/{expense}/edit', [ExpenseController::class, 'edit'])->name('expense.edit');
+// Route::patch('expenses/{expense}', [ExpenseController::class, 'update'])->name('expense.update');
+// Route::delete('expenses/{expense}', [ExpenseController::class, 'destroy'])->name('expense.destroy');
 
+// });
+// Expense related routes
+Route::middleware('auth')->prefix('expenses')->name('expense.')->group(function () {
+    Route::controller(ExpenseController::class)->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/create', 'create')->name('create');
+        Route::post('expenses', 'store')->name('store');
+        Route::get('/{expense}', 'show')->name('show');
+        Route::get('/{expense}/edit', 'edit')->name('edit');
+        Route::patch('/{expense}', 'update')->name('update');
+        Route::delete('/{expense}', 'destroy')->name('destroy');
+        Route::patch('/{expense}/status/{status}')->name('updateStatus');
+    });
+});
+Route::name('notification.')->group(function () {
+    Route::controller(NotificationController::class)->group(function () {
+        Route::get('/notifications', 'allNotifications')->name('allNotifications');
+        Route::get('/notifications/{id}', 'read')->name('read');
+        Route::get('/notifications/readAll', 'markAllAsRead')->name('readAll');
+    });
 });
 require __DIR__.'/auth.php';
