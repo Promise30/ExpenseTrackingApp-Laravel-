332e2bbab95025cfd3450fc2998a503042e9df43
Promise30 <promiseadeagbo@gmail.com>
2024-06-09 18:59:59 +0000

    updated styling

--- resources/views/expenses/show.blade.php
+++ resources/views/expenses/show.blade.php
@@ -1,45 +1,109 @@
 <x-app-layout>
 
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-        <h1 class="text-dark text-lg font-bold">Expense: {{ $expense->title }}</h1>
-        <div class="w-full sm:max-w-xl mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-            <div class="text-dark flex flex-col py-4">
-                <p>Description: {{$expense->description}}</p>
-                <hr>
-                <p class="mt-2">Quantity: {{$expense->quantity}} units</p>
-                <hr>
-                <p class="mt-2">Unit Price: # {{$expense->unit_price}}</p>
-                <hr>
-                <p class="mt-2">Total Cost: # {{$expense->getTotalCostAttribute()}}</p>
-                <hr>
-                <p class="mt-2">Category: {{$expense->category}}</p>
-                <hr>
-                <p class="mt-2">Date Created: {{ $expense->created_at->format('Y-m-d H:i:s') }}</p>
-                <hr>
-                @if ($expense->receipt)
-                <p class="mt-2">Receipt:
-                    <a href="{{ '/storage/' . $expense->receipt }}" target="_blank">Receipt</a>
-                </p>
-                @endif
-            </div>
+    <div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto">
+        @if($expense->receipt)
+        <a href="{{ '/storage/' . $expense->receipt }}" target="_blank">
+            <img class="h-48 w-full object-cover object-top" src="{{ '/storage/' . $expense->receipt }}" alt="Expense Receipt">
+        </a>
+        @endif
+        <div class="px-6 py-2">
+          <div class="font-bold text-xl mb-2">{{$expense->title}}</div>
+          <p class="text-gray-700 text-base">
+           {{$expense->description}}
+          </p>
+          <hr>
+          <p class="text-gray-700 text-base">
+            Category: {{$expense->category}}
+           </p>
+           <hr>
+          <p class="text-gray-700 text-base">
+            Quantity: {{$expense->quantity}} units
+          </p>
+          <hr>
+          <p class="text-gray-700 text-base">
+            Unit Price: {{$expense->unit_price}}
+          </p>
+          <hr>
+          <p class="text-gray-700 text-base">
+            Total Cost: # {{$expense->getTotalCostAttribute()}}
+          </p>
+          <hr>
+          <p class="text-gray-700 text-base">
+            Date Created: {{$expense->created_at->format('Y-m-d')}}
+          </p>
+          <hr>
+          <p class="text-gray-700 text-base">
+            Status: {{$expense->status}}
+          </p>
         </div>
-        <div class="flex justify-between mt-2 py-2">
-            <div class="flex ">
-                <a href="{{ route('expense.index') }}" class="mr-4">
-                    <x-primary-button>Back</x-primary-button>
-                </a>
-                <a href="{{ route('expense.edit', $expense->id) }}" class="mr-2">
-                    <x-primary-button>Edit</x-primary-button>
-                </a>
+        <div class="px-6 pb-2">
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            <a href="{{ route('expense.index') }}" >
+                <x-primary-button>Back</x-primary-button>
+            </a>
+          </span>
+          {{-- @if (Auth::user()->hasRole('Administrator'))
+          <span class="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold text-gray-500  mb-2"> --}}
+            {{-- <a href="{{ route('expense.edit', $expense->id) }}" > 
+              <form action="{{route('expense.update', $expense->id)}}" method="POST" enctype="multipart/form-data">
+                @csrf
+                @method('patch')
+                <input type="hidden" name="status" value="approved" />
+                <button type="submit">Approve</button>
+              </form>
+                
+             </a> 
+          </span>
+          <span class="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold text-gray-500  mb-2">
+             {{-- <a href="{{ route('expense.edit', $expense->id) }}" >  --}}
+              {{-- <form method="POST" action="{{ route('expense.update', $expense->id) }}" enctype="multipart/form-data">
+                @csrf
+                @method('patch')
+                <input type="hidden" name="status" value="rejected" />
+                <button type="submit">Reject</button>
+              </form>
+                 --}}
+            {{-- </a>  --}}
+          {{-- </span> --}}
+          {{-- @if (Auth::user()->hasRole('Administrator'))
+    <a href="{{ route('expense.update', ['expense' => $expense->id, 'status' => 'approved']) }}"
+       class="inline-block bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+       onclick="event.preventDefault(); document.getElementById('approve-form').submit();">
+        Approve
+    </a>
 
-                <form class="ml-2" action="{{ route('expense.destroy', $expense->id) }}" method="post">
-                    @method('delete')
-                    @csrf
-                    <x-primary-button>Delete</x-primary-button>
-                </form>
-            </div>
+    <form id="approve-form" action="{{ route('expense.update', ['expense' => $expense->id, 'status' => 'approved']) }}" method="POST" style="display: none;">
+        @csrf
+        @method('PATCH')
+    </form>
+
+    <a href="{{ route('expense.updateStatus', ['expense' => $expense->id, 'status' => 'rejected']) }}"
+       class="inline-block bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+       onclick="event.preventDefault(); document.getElementById('reject-form').submit();">
+        Reject
+    </a>
+
+    <form id="reject-form" action="{{ route('expense.updateStatus', ['expense' => $expense->id, 'status' => 'rejected']) }}" method="POST" style="display: none;">
+        @csrf
+        @method('PATCH')
+    </form> --}}
+              
+          {{-- @else --}}
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            <a href="{{ route('expense.edit', $expense->id) }}" >
+                <x-primary-button>Edit</x-primary-button>
+            </a>
+          </span>
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            <form action="{{ route('expense.destroy', $expense->id) }}" method="post">
+                @method('delete')
+                @csrf
+                <x-primary-button>Delete</x-primary-button>
+            </form>
+          </span>
+          {{-- @endif --}}
+          
         </div>
+      </div>
     </div>
-
-
 </x-app-layout>
